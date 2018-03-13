2db44c5def327df7c029b65711c8a9050e5b2b1e
andrew.wang <awngas@163.com>
2018-03-13 10:27:32 +0800

    gitignore

--- study/自定义模型/第14章例子_船/ships.py
+++ study/自定义模型/第14章例子_船/ships.py
@@ -11,20 +11,20 @@ NAME, OWNER, COUNTRY, DESCRIPTION, TEU = range(5)
 MAGIC_NUMBER = 0x570C4
 FILE_VERSION = 1
 
-#每条船都可以用Ship来表示
+# 每条船都可以用Ship来表示
 class Ship(object):
     def __init__(self, name, owner, country, teu=0, description=""):
-        self.name = name #名字
-        self.owner = owner   #所属人
-        self.country = country #国家
-        self.teu = teu #该船可以放下多少标准集装箱
-        self.description = description #描述信息
+        self.name = name            # 名字
+        self.owner = owner          # 所属人
+        self.country = country      # 国家
+        self.teu = teu              # 该船可以放下多少标准集装箱
+        self.description = description  # 描述信息
 
-    #魔术方法:定义hash()方法返回的值
+    # 魔术方法:定义hash()方法返回的值
     def __hash__(self):
         return super(Ship, self).__hash__()
 
-    #魔术方法:定义小于行为
+    # 魔术方法:定义小于行为
     def __lt__(self, other):
         return bool(self.name.lower())
 
@@ -32,14 +32,14 @@ class Ship(object):
     def __eq__(self, other):
         return bool(self.name.lower() == other.name.lower())
 
-
+# 自定义模型
 class ShipTableModel(QAbstractTableModel):
     dataChanged = pyqtSignal(QModelIndex, QModelIndex)
 
     def __init__(self, filename=""):
         super(ShipTableModel, self).__init__()
         self.filename = filename
-        self.dirty = False
+        self.dirty = False  # 标识model数据是否做过等该
         self.ships = []
         self.owners = set()
         self.countries = set()
@@ -55,13 +55,16 @@ class ShipTableModel(QAbstractTableModel):
                             key=lambda x: (x.country, x.owner, x.name))
         self.endResetModel()
 
+    # 自定义可编辑模型时需要实现: 返回给定索引的项目标志。
     def flags(self, index):
-        if not index.isValid():
+        if not index.isValid():  # 索引无效返回Qt.ItemIsEnabled标识
             return Qt.ItemIsEnabled
-        return Qt.ItemFlags(
+        return Qt.ItemFlags(     # 索引有效,在原有标识基础上添加一个Qt.ItemIsEditable(可编辑标识)
             QAbstractTableModel.flags(self, index) |
             Qt.ItemIsEditable)
 
+    # 自定义模型必须实现: 返回给定索引引用的项目和给定的角色下存储的数据。
+    # 有关角色见 https://doc.qt.io/qt-5/qt.html#ItemDataRole-enum
     def data(self, index, role=Qt.DisplayRole):
         if (not index.isValid() or
                 not (0 <= index.row() < len(self.ships))):
@@ -79,7 +82,7 @@ class ShipTableModel(QAbstractTableModel):
                 return ship.description
             elif column == TEU:
                 return ship.teu
-        elif role == Qt.TextAlignmentRole:
+        elif role == Qt.TextAlignmentRole:  # 项目的文本对齐方式
             if column == TEU:
                 return QVariant(int(Qt.AlignRight | Qt.AlignVCenter))
             return QVariant(int(Qt.AlignLeft | Qt.AlignVCenter))
@@ -104,14 +107,16 @@ class ShipTableModel(QAbstractTableModel):
                 return QVariant(QColor(210, 230, 230))
         return QVariant()
 
+    # 以指定的方向返回标题中给定角色和部分的数据。
+    # 当orientation是Qt.Vertical时,section会是行偏移,当是Qt.Horizontal时,section是列偏移
     def headerData(self, section, orientation, role=Qt.DisplayRole):
         if role == Qt.TextAlignmentRole:
-            if orientation == Qt.Horizontal:
+            if orientation == Qt.Horizontal:  # 列标题左对齐
                 return QVariant(int(Qt.AlignLeft | Qt.AlignVCenter))
-            return QVariant(int(Qt.AlignRight | Qt.AlignVCenter))
+            return QVariant(int(Qt.AlignRight | Qt.AlignVCenter))  # 行标题右对齐
         if role != Qt.DisplayRole:
             return QVariant()
-        if orientation == Qt.Horizontal:
+        if orientation == Qt.Horizontal: # 列偏移从1开始
             if section == NAME:
                 return QVariant("Name")
             elif section == OWNER:
@@ -124,17 +129,21 @@ class ShipTableModel(QAbstractTableModel):
                 return QVariant("TEU")
         return QVariant(int(section + 1))
 
+    # 自定义模型必须实现: 返回行数
     def rowCount(self, index=QModelIndex()):
         return len(self.ships)
 
+    # 自定义模型必须实现: 返回列数
     def columnCount(self, index=QModelIndex()):
         return 5
 
+    # 自定义可编辑模型时需要实现: 将索引处的项目的角色数据设置为值。
+    # 这里忽略了角色,
     def setData(self, index, value, role=Qt.EditRole):
         if index.isValid() and 0 <= index.row() < len(self.ships):
             # self.beginResetModel()
-            ship = self.ships[index.row()]
-            column = index.column()
+            ship = self.ships[index.row()]  # 根据行号获取数据
+            column = index.column()  # 取得列
             if column == NAME:
                 ship.name = str(value)
             elif column == OWNER:
@@ -143,7 +152,7 @@ class ShipTableModel(QAbstractTableModel):
                 ship.country = str(value)
             elif column == DESCRIPTION:
                 ship.description = str(value)
-            elif column == TEU:
+            elif column == TEU:  # 如果是TEU列,判断是否是数字,是数字才存储
                 if str(value).isdecimal():
                     ship.teu = int(value)
             self.dirty = True
@@ -152,10 +161,12 @@ class ShipTableModel(QAbstractTableModel):
             return True
         return False
 
+    # 自定义可编辑模型时需要实现: 插入一行或多行,position插入行的位置
     def insertRows(self, position, rows=1, index=QModelIndex()):
         self.beginInsertRows(QModelIndex(), position, position + rows - 1)
         for row in range(rows):
-            self.ships.insert(position + row,
+            self.ships.insert(position + row
+                              ,
                               Ship(" Unknown", " Unknown", " Unknown"))
         self.endInsertRows()
         self.dirty = True
--- study/自定义模型/第14章例子_船/自定义模型_船.py
+++ study/自定义模型/第14章例子_船/自定义模型_船.py
@@ -60,20 +60,22 @@ class MainForm(QDialog):
         layout.addLayout(buttonLayout)
         self.setLayout(layout)
 
+        # 为每个水平标题连接到sortTable方法上
         for tableView in (self.tableView1, self.tableView2):
-            header = tableView.horizontalHeader() #返回水平标题,QHeaderView
-            header.sectionClicked[int].connect(self.sortTable) #点击标题绑定信号,点击某个部分时会发出此信号。该部分的逻辑索引由logicalIndex指定
+            header = tableView.horizontalHeader()  # 返回水平标题,QHeaderView
+            header.sectionClicked[int].connect(self.sortTable)  # 点击标题绑定信号,点击某个部分时会发出此信号。该部分的逻辑索引由logicalIndex指定
+
         addShipButton.clicked.connect(self.addShip)
         removeShipButton.clicked.connect(self.removeShip)
         quitButton.clicked.connect(self.accept)
 
         self.setWindowTitle("Ships (model)")
-        QTimer.singleShot(0, self.initialLoad)
+        QTimer.singleShot(0, self.initialLoad)  # 设置定时器,启动时间0,调用initialLoad方法初始化数据
 
     def initialLoad(self):
         if not QFile.exists(self.model.filename):
             self.model.beginResetModel()
-            for ship in ships.generateFakeShips(): #generateFakeShips构造Ship对象列表
+            for ship in ships.generateFakeShips():  # generateFakeShips构造Ship对象列表
                 self.model.ships.append(ship)
                 self.model.owners.add(str(ship.owner))
                 self.model.countries.add(str(ship.country))
@@ -88,15 +90,19 @@ class MainForm(QDialog):
         self.model.sortByName()
         self.resizeColumns()
 
+    # 对两个表格视图NAME,OWNER,COUNTRY,TEU列,按照内容改变列大小
     def resizeColumns(self):
         for tableView in (self.tableView1, self.tableView2):
             for column in (ships.NAME, ships.OWNER, ships.COUNTRY,
                            ships.TEU):
                 tableView.resizeColumnToContents(column)
 
+    # QDialog.reject() 隐藏模式对话框并将result code 设置为Rejected 拒绝
     def reject(self):
+        print()
         self.accept()
 
+    # QDialog.accept() 隐藏模式对话框并将result code 设置为Accepted 接受
     def accept(self):
         if (self.model.dirty and
                     QMessageBox.question(self, "Ships - Save?",
@@ -110,6 +116,7 @@ class MainForm(QDialog):
                                     "Failed to save: {0}".format(e))
         QDialog.accept(self)
 
+    #table视图排序方法
     def sortTable(self, section):
         if section in (ships.OWNER, ships.COUNTRY):
             self.model.sortByCountryOwner()
@@ -117,28 +124,32 @@ class MainForm(QDialog):
             self.model.sortByName()
         self.resizeColumns()
 
+    # 槽方法,添加一个船
     def addShip(self):
-        row = self.model.rowCount()
-        self.model.insertRows(row)
-        index = self.model.index(row, 0)
+        row = self.model.rowCount()  # 获取模型数据条数
+        self.model.insertRows(row)   # 在最后插入一条
+        index = self.model.index(row, 0) # 获得插入的index
+        # 判断键盘焦点在哪个表格视图上
         tableView = self.tableView1
         if self.tableView2.hasFocus():
             tableView = self.tableView2
-        tableView.setFocus()
-        tableView.setCurrentIndex(index)
-        tableView.edit(index)
+        tableView.setFocus()  # 设置焦点
+        tableView.setCurrentIndex(index) # 设置当前行
+        tableView.edit(index) # 编辑当前行
 
+    # 槽方法,删除一个船
     def removeShip(self):
+        # 判断键盘焦点在哪个表格视图上
         tableView = self.tableView1
         if self.tableView2.hasFocus():
             tableView = self.tableView2
         index = tableView.currentIndex()
-        if not index.isValid():
+        if not index.isValid():  # PyQt5.QtCore.QModelIndex.isValid() 此模型索引有效返回true,无效返回false
             return
-        row = index.row()
+        row = index.row()   # 返回此模型索引引用的行
         name = self.model.data(
-            self.model.index(row, ships.NAME))
-        owner = self.model.data(
+            self.model.index(row, ships.NAME))   # QModelIndex QAbstractTableModel.index() 返回指定行,列的索引
+        owner = self.model.data(                 # QVariant QAbstractItemModel.data() 返回索引所引用项目的给定角色下存储的数据
             self.model.index(row, ships.OWNER))
         country = self.model.data(
             self.model.index(row, ships.COUNTRY))
